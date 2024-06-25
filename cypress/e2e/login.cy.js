ba19f9d19bcdac9851f540dd4e43975b7d218328
Andrzej Szomszor <andrzej.szomszor@brainly.com>
2024-06-25 16:29:21 +0200

    update tests

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -7,6 +7,14 @@ describe("Login Test", () => {
     cy.get("button[id=logout]").should("be.visible");
     cy.get("button[id=add-contact]").should("be.visible");
   });
+  it("Visits the login page and logs in 2", () => {
+    cy.visit("https://thinking-tester-contact-list.herokuapp.com");
+    cy.get("input[id=email]").type("martamentee@test.com");
+    cy.get("input[id=password]").type("Test11!!");
+    cy.get("form").submit();
+    cy.get("button[id=logout]").should("be.visible");
+    cy.get("button[id=add-contact]").should("be.visible");
+  });
   afterEach(() => {
     cy.get("button[id=logout]").click();
   });
